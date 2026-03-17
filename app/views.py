817f300f744caa230fcc6c602485230536ce6aca
Sheldon Jones <sheldonajones@yahoo.com>
2026-03-16 20:19:04 -0500

    Exercise 4 complete

--- app/views.py
+++ app/views.py
@@ -9,13 +9,25 @@ from werkzeug.security import check_password_hash
 from app.forms import UploadForm
 
 
+ALLOWED_EXTENSIONS = {".jpg", ".jpeg", ".png"}
+def get_uploaded_images(upload_folder):
+    image_files = []
+
+    for subdir, _dirs, files in os.walk(upload_folder):
+        for file in files:
+            _root, extension = os.path.splitext(file)
+            if extension.lower() in ALLOWED_EXTENSIONS:
+                image_files.append(os.path.join(subdir, file))
+
+    return [os.path.basename(path) for path in image_files]
 ###
 # Routing for your application.
 ###
 
-@app.route('/')
-def home():
-    """Render website's home page."""
+def init_routes(app):
+    @app.route('/')
+    def home():
+     """Render website's home page."""
     return render_template('home.html')
 
 
@@ -42,11 +54,14 @@ def upload():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+
+    if current_user.is_authenticated:
+            return redirect(url_for("upload"))
     form = LoginForm()
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
+    if form.validate_on_submit():
         # Get the username and password values from the form.
 
         # Using your model, query database for a user based on the username
@@ -54,13 +69,30 @@ def login():
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
+        user = UserProfile.query.filter_by(username=form.username.data).first()
 
+        if user and check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You were successfully logged in.", "success")
+                return redirect(url_for("upload"))
+
+        flash("Invalid username or password.", "danger")
         # Gets user id, load into session
         login_user(user)
 
         # Remember to flash a message to the user
         return redirect(url_for("home"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
+
+
+
+                    
+
+               
+            
+
+        return render_template("login.html", form=form)
+
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
