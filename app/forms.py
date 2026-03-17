a5e9fb8e5ba6b69c0d3ac2dce287c0058d67dee3
Sheldon Jones <sheldonajones@yahoo.com>
2026-03-16 19:21:03 -0500

    migration done

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired
-
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
